4173523f5356e87a06a9d29d574d3d1d154b13e9
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-08-30 23:37:49 +0700

    chore: update using extension to calculate total sales

--- lib/repositories/order_repository.dart
+++ lib/repositories/order_repository.dart
@@ -1,19 +1,16 @@
 import 'package:admin_ecommerce_app/constants/firebase_constants.dart';
+import 'package:admin_ecommerce_app/extensions/list_order_extension.dart';
 import 'package:admin_ecommerce_app/models/order.dart';
 
 class OrderRepository {
   Future<Map<String, double>> getSalesStatistics() async {
     final snapshot = await ordersRef.get();
+    final orders = snapshot.docs
+        .map((e) => OrderModel.fromMap(e.data() as Map<String, dynamic>))
+        .toList();
     return {
       'total_orders': snapshot.docs.length.toDouble(),
-      'total_sales': snapshot.docs.isEmpty
-          ? 0
-          : snapshot.docs.fold(
-              0,
-              (previousValue, element) =>
-                  previousValue +
-                  (element.data() as Map<String, dynamic>)['orderSummary']
-                      ['total']),
+      'total_sales': orders.totalSales,
     };
   }
 
