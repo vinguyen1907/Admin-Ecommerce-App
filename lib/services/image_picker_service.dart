5f0006089c1d52a9290004443b0721fd7cfdf239
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-09-15 14:21:27 +0700

    chore: remove using image picker web package

--- lib/services/image_picker_service.dart
+++ lib/services/image_picker_service.dart
@@ -1,12 +1,8 @@
 import 'dart:typed_data';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'image_picker_service_mobile.dart'
     if (dart.library.io) 'image_picker_service_mobile.dart';
-import 'image_picker_service_web.dart'
-    if (dart.library.html) 'image_picker_service_web.dart';
 
-final ImagePickerService imagePickerService =
-    kIsWeb ? ImagePickerServiceWeb() : ImagePickerServiceMobile();
+final ImagePickerService imagePickerService = ImagePickerServiceMobile();
 
 abstract class ImagePickerService {
   Future<Uint8List?> pickImage();
