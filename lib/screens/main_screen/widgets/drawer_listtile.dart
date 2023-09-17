2c7739639ab8670a5ca6cb84288bb84c67320dc5
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-09-17 08:55:03 +0700

    chore: update sidebar to adapt with tablet size

--- lib/screens/main_screen/widgets/drawer_listtile.dart
+++ lib/screens/main_screen/widgets/drawer_listtile.dart
@@ -1,5 +1,6 @@
 import 'package:admin_ecommerce_app/common_widgets/my_icon.dart';
 import 'package:admin_ecommerce_app/constants/app_styles.dart';
+import 'package:admin_ecommerce_app/responsive.dart';
 import 'package:flutter/material.dart';
 
 class DrawerListTile extends StatelessWidget {
@@ -20,14 +21,29 @@ class DrawerListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        onTap: onTap,
-        leading: MyIcon(icon: isSelected ? selectedIcon : icon),
-        title: Text(
-          title,
-          style: isSelected
-              ? AppStyles.drawerListTileTitleSelected
-              : AppStyles.drawerListTileTitle,
-        ));
+    if (!Responsive.isTablet(context)) {
+      return ListTile(
+          onTap: onTap,
+          leading: MyIcon(
+            icon: isSelected ? selectedIcon : icon,
+            width: 20,
+          ),
+          minLeadingWidth: 20,
+          title: Text(
+            title,
+            style: isSelected
+                ? AppStyles.drawerListTileTitleSelected
+                : AppStyles.drawerListTileTitle,
+          ));
+    } else if (Responsive.isTablet(context)) {
+      return IconButton(
+          onPressed: onTap,
+          icon: MyIcon(
+            icon: isSelected ? selectedIcon : icon,
+            width: 20,
+          ));
+    } else {
+      return const SizedBox();
+    }
   }
 }
