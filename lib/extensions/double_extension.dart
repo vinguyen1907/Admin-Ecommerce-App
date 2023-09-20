434384ba9fee295d546a5c6b3e220325a826b8db
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-09-20 20:34:23 +0700

    fix: disable scrolling on pie chart list, get fixed string on pie chart data

--- lib/extensions/double_extension.dart
+++ lib/extensions/double_extension.dart
@@ -2,4 +2,8 @@ extension DoubleExtensions on double {
   String toPriceString() {
     return "\$${toStringAsFixed(2)}";
   }
+
+  String toFixedString(int numberOfDigitsAfterComma) {
+    return "${toStringAsFixed(2)}%";
+  }
 }
