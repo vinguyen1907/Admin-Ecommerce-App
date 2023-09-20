26df9c3a9b3c1d58f395e321c998967535444bf1
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-09-20 20:04:17 +0700

    fix: next and previous promotion page

--- lib/repositories/promotion_repository.dart
+++ lib/repositories/promotion_repository.dart
@@ -27,6 +27,19 @@ class PromotionRepository {
     }
   }
 
+  Future<int> fetchTotalPromotionCount() async {
+    try {
+      final snapshot = await promotionsRef
+          .where("isDeleted", isEqualTo: false)
+          .count()
+          .get();
+      final count = snapshot.count;
+      return count;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
   Future<PromotionsWithLastDoc> fetchMorePromotions(
       {required DocumentSnapshot lastDocument, required int limit}) async {
     try {
