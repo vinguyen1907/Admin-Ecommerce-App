0e06477924288fb05eb4de918e329b7a6f00d329
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2023-09-20 14:07:23 +0700

    fix: change size image picker

--- lib/screens/add_category_screen/add_category_screen.dart
+++ lib/screens/add_category_screen/add_category_screen.dart
@@ -3,6 +3,7 @@ import 'package:admin_ecommerce_app/common_widgets/image_picker_widget.dart';
 import 'package:admin_ecommerce_app/common_widgets/my_elevated_button.dart';
 import 'package:admin_ecommerce_app/common_widgets/my_text_form_field.dart';
 import 'package:admin_ecommerce_app/common_widgets/primary_background.dart';
+import 'package:admin_ecommerce_app/common_widgets/screen_horizontal_padding_widget.dart';
 import 'package:admin_ecommerce_app/common_widgets/screen_name_section.dart';
 import 'package:admin_ecommerce_app/constants/app_dimensions.dart';
 import 'package:admin_ecommerce_app/constants/app_styles.dart';
@@ -36,6 +37,8 @@ class _AddCategoryScreenState extends State<AddCategoryScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    double width =
+        !Responsive.isDesktop(context) ? size.width * 0.35 : size.width * 0.1;
     return SafeArea(
       child: SingleChildScrollView(
         child: Container(
@@ -57,49 +60,51 @@ class _AddCategoryScreenState extends State<AddCategoryScreen> {
                         _showSubmitSuccessful();
                       }
                     },
-                    child: PrimaryBackground(
-                      margin: Responsive.isDesktop(context)
-                          ? EdgeInsets.zero
-                          : const EdgeInsets.symmetric(
+                    child: ScreenHorizontalPaddingWidget(
+                      child: PrimaryBackground(
+                        margin: Responsive.isDesktop(context)
+                            ? EdgeInsets.zero
+                            : const EdgeInsets.symmetric(
+                                horizontal: AppDimensions
+                                    .mobileHorizontalContentPadding),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
                               horizontal:
                                   AppDimensions.mobileHorizontalContentPadding),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal:
-                                AppDimensions.mobileHorizontalContentPadding),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            MyTextFormField(
-                              controller: _nameController,
-                              hintText: 'Category name',
-                              label: 'Category name',
-                              validator: ValidatorUtils.validateText,
-                            ),
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Image',
-                                  style: AppStyles.headlineMedium,
-                                ),
-                                ImagePickerWidget(
-                                  height: size.width * 0.35 * 1.3,
-                                  width: size.width * 0.35,
-                                  image: _image,
-                                  onTap: _addImage,
-                                ),
-                              ],
-                            ),
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: MyElevatedButton(
-                                  onPressed: () => _addCategory(),
-                                  widget: const Text('Submit')),
-                            )
-                          ],
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              MyTextFormField(
+                                controller: _nameController,
+                                hintText: 'Category name',
+                                label: 'Category name',
+                                validator: ValidatorUtils.validateText,
+                              ),
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Image',
+                                    style: AppStyles.headlineMedium,
+                                  ),
+                                  ImagePickerWidget(
+                                    height: width * 1.3,
+                                    width: width,
+                                    image: _image,
+                                    onTap: _addImage,
+                                  ),
+                                ],
+                              ),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: MyElevatedButton(
+                                    onPressed: () => _addCategory(),
+                                    widget: const Text('Submit')),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ))
--- lib/screens/edit_category_screen/edit_category_screen.dart
+++ lib/screens/edit_category_screen/edit_category_screen.dart
@@ -7,6 +7,7 @@ import 'package:admin_ecommerce_app/common_widgets/image_picker_widget.dart';
 import 'package:admin_ecommerce_app/common_widgets/my_elevated_button.dart';
 import 'package:admin_ecommerce_app/common_widgets/my_text_form_field.dart';
 import 'package:admin_ecommerce_app/common_widgets/primary_background.dart';
+import 'package:admin_ecommerce_app/common_widgets/screen_horizontal_padding_widget.dart';
 import 'package:admin_ecommerce_app/common_widgets/screen_name_section.dart';
 import 'package:admin_ecommerce_app/constants/app_dimensions.dart';
 import 'package:admin_ecommerce_app/constants/app_styles.dart';
@@ -42,6 +43,8 @@ class _EditCategoryScreenState extends State<EditCategoryScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    double width =
+        !Responsive.isDesktop(context) ? size.width * 0.35 : size.width * 0.1;
     return BlocConsumer<EditCategoryScreenBloc, EditCategoryScreenState>(
       listener: (context, state) {
         if (state is Updating) {
@@ -66,49 +69,51 @@ class _EditCategoryScreenState extends State<EditCategoryScreen> {
                   children: [
                     const ScreenNameSection("Edit Category",
                         hasDefaultBackButton: true, margin: EdgeInsets.zero),
-                    PrimaryBackground(
-                      margin: Responsive.isDesktop(context)
-                          ? EdgeInsets.zero
-                          : const EdgeInsets.symmetric(
+                    ScreenHorizontalPaddingWidget(
+                      child: PrimaryBackground(
+                        margin: Responsive.isDesktop(context)
+                            ? EdgeInsets.zero
+                            : const EdgeInsets.symmetric(
+                                horizontal: AppDimensions
+                                    .mobileHorizontalContentPadding),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
                               horizontal:
                                   AppDimensions.mobileHorizontalContentPadding),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal:
-                                AppDimensions.mobileHorizontalContentPadding),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            MyTextFormField(
-                              controller: _nameController,
-                              hintText: 'Category name',
-                              label: 'Category name',
-                              validator: ValidatorUtils.validateText,
-                            ),
-                            Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Image',
-                                  style: AppStyles.headlineMedium,
-                                ),
-                                ImagePickerWidget(
-                                  height: size.width * 0.35 * 1.3,
-                                  width: size.width * 0.35,
-                                  image: state.imageSelected,
-                                  onTap: _addImage,
-                                ),
-                              ],
-                            ),
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: MyElevatedButton(
-                                  onPressed: () => _updateCategory(),
-                                  widget: const Text('Update')),
-                            )
-                          ],
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              MyTextFormField(
+                                controller: _nameController,
+                                hintText: 'Category name',
+                                label: 'Category name',
+                                validator: ValidatorUtils.validateText,
+                              ),
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Image',
+                                    style: AppStyles.headlineMedium,
+                                  ),
+                                  ImagePickerWidget(
+                                    height: width * 1.3,
+                                    width: width,
+                                    image: state.imageSelected,
+                                    onTap: _addImage,
+                                  ),
+                                ],
+                              ),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: MyElevatedButton(
+                                    onPressed: () => _updateCategory(),
+                                    widget: const Text('Update')),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     )
